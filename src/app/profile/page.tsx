34a0f4017d87cc116e039a22ff9fe393f693e992
ben wainaina <bwainainamwangi@gmail.com>
2024-09-28 14:46:48 +0300

    a bit of cleanup

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,8 +6,6 @@ import darkProfilePic from "../assets/dark-profile.png";
 import lightProfilePic from "../assets/light-profile.png";
 import { selectCurrentMode } from "../state-manager/selectors";
 import { useSelector } from "react-redux";
-import { IAppThemeMode } from "../state-manager/interfaces";
-import { useEffect, useState } from "react";
 
 export default function Profile() {
   /**
@@ -15,25 +13,14 @@ export default function Profile() {
    */
   const currentMode = useSelector(selectCurrentMode);
 
-  /**
-   * states
-   */
-  const [mode, setMode] = useState<IAppThemeMode>("light");
-
-  /**
-   * effects
-   */
-  useEffect(() => {
-    setMode(currentMode);
-  }, [currentMode]);
-
   return (
-    <div className={`profile-wrapper poppins-bold ${mode}`}>
+    <div className={`profile-wrapper poppins-bold ${currentMode}`}>
       <h4>Welcome back</h4>
       <h1>benson</h1>
       <Image
-        src={mode === "light" ? lightProfilePic : darkProfilePic}
-        alt={`profile image ${mode}`}
+        priority={true}
+        src={currentMode === "light" ? lightProfilePic : darkProfilePic}
+        alt={`profile image ${currentMode}`}
       />
       <h3>You stop growing when you stop learning.</h3>
     </div>
